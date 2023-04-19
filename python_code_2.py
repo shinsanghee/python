b8694a165026d79e91cc22265a0d7f396e6ef944
sanghee Shin <113161106+shinsanghee@users.noreply.github.com>
2023-04-19 13:51:09 +0900

    Create python_code_2.py

---
+++ python_code_2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Jupyter Notebook: Edit mode")
+
+
+# In[2]:
+
+
+print("Jupyter Notebook: Command mode")
+
+
+# In[3]:
+
+
+print('Hello Jupyter Notebook!!')
+
+
+# In[4]:
+
+
+print('Hello Jupyter Notebook!!')
+
+
+# In[5]:
+
+
+print('Hello Jupyter Notebook!!')
+
+
+# In[6]:
+
+
+print('Jupyter Notebook: code line 1')
+
+
+# In[7]:
+
+
+print('Jupyter Notebook: code line 2')
+
+
+# In[8]:
+
+
+print('Jupyter Notebook: code line 1') 
+print('Jupyter Notebook: code line 2') 
+print('Jupyter Notebook: code line 3')
+
+
+# In[ ]:
+
+
+
+
